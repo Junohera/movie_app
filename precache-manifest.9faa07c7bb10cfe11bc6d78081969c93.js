8c86a9bbf0d35f8795d55566018db58dbdde95f5
Juno <juno.address@gmail.com>
2019-01-30 20:22:43 +0900

    Updates

---
+++ precache-manifest.9faa07c7bb10cfe11bc6d78081969c93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "e1de84ff69e5712869dc",
+    "url": "/movie_app/static/js/main.e1de84ff.chunk.js"
+  },
+  {
+    "revision": "d120fdf0ddba1865b329",
+    "url": "/movie_app/static/js/1.d120fdf0.chunk.js"
+  },
+  {
+    "revision": "e1de84ff69e5712869dc",
+    "url": "/movie_app/static/css/main.12a6d307.chunk.css"
+  },
+  {
+    "revision": "eaaee7990329b75a1c6e9f981e13d6e7",
+    "url": "/movie_app/index.html"
+  }
+];
